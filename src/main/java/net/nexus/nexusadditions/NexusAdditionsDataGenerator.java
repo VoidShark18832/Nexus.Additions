3a8c967c6ede212ada81f3e446c209fa43a98f40
Cody Simmons <codysimmons2011@gmail.com>
2026-09-05 16:06:45 +0100

    Recipes Added

--- src/main/java/net/nexus/nexusadditions/NexusAdditionsDataGenerator.java
+++ src/main/java/net/nexus/nexusadditions/NexusAdditionsDataGenerator.java
@@ -2,11 +2,14 @@ package net.nexus.nexusadditions;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.nexus.nexusadditions.datagen.ModRecipeProvider;
 import org.jspecify.annotations.NonNull;
 
 public class NexusAdditionsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(@NonNull FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
