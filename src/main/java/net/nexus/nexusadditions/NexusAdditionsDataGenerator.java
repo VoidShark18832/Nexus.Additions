2f83ae9c8ee29ff42e3f18f58a35f8ce1e9727e4
Cody Simmons <codysimmons2011@gmail.com>
2026-09-05 13:46:49 +0100

    First Commit - Uploaded Project with Datagen premade

---
+++ src/main/java/net/nexus/nexusadditions/NexusAdditionsDataGenerator.java
@@ -0,0 +1,12 @@
+package net.nexus.nexusadditions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jspecify.annotations.NonNull;
+
+public class NexusAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(@NonNull FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/nexus/nexusadditions/datagen/ModRecipeProvider.java
@@ -0,0 +1,36 @@
+package net.nexus.nexusadditions.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeProvider createRecipeProvider(HolderLookup.Provider registries, RecipeOutput output) {
+        return new RecipeProvider(registries, output) {
+            @Override
+            public void buildRecipes() {
+                List<Item> FixedCrafingSticks = Collections.singletonList(Items.OAK_PLANKS);
+
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "Nexus Additions Recipes";
+    }
+}
