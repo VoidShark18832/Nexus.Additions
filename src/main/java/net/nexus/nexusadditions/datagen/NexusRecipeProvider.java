3117830776da3f1a1d46d07d48da8355ff903d7e
Cody Simmons <codysimmons2011@gmail.com>
2026-09-06 19:30:01 +0100

    Fixed Stuff

--- src/main/java/net/nexus/nexusadditions/datagen/NexusRecipeProvider.java
+++ src/main/java/net/nexus/nexusadditions/datagen/NexusRecipeProvider.java
@@ -1,16 +1,11 @@
 package net.nexus.nexusadditions.datagen;
 
-import java.io.File;
 import java.util.concurrent.CompletableFuture;
-
 import net.minecraft.core.HolderLookup;
-
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.tags.ItemTags;
-
-
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.world.item.Items;
@@ -24,21 +19,110 @@ public class NexusRecipeProvider extends FabricRecipeProvider {
         return new RecipeProvider(registryLookup, exporter) {
             @Override
             public void buildRecipes() {
+                // TABLES
                 shaped(RecipeCategory.MISC, Items.CRAFTING_TABLE, 4)
                         .pattern("PP")
                         .pattern("PP")
                         .define('P', ItemTags.PLANKS)
                         .group("multi_bench")
-                        .unlockedBy(getHasName(Items.CRAFTING_TABLE), has(Items.CRAFTING_TABLE))
-                        .save(output, "planks_to_crafting_table");
+                        .unlockedBy(getHasName(Items.CRAFTING_TABLE), has(Items.CRAFTING_TABLE));
+                // ITEMS
                 shaped(RecipeCategory.MISC, Items.STICK, 64)
                         .pattern("P")
                         .pattern("P")
                         .define('P', ItemTags.PLANKS)
-                        .group("crafting")
+                        .group("Crafting")
                         .unlockedBy(getHasName(Items.STICK), has(Items.STICK))
-                        .save(output, "minecraft:stick");
-
+                        .save(output);
+                // STAIRS
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.OAK_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.OAK_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.OAK_PLANKS), has(Items.OAK_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.SPRUCE_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.SPRUCE_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.SPRUCE_PLANKS), has(Items.SPRUCE_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.JUNGLE_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.JUNGLE_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.JUNGLE_PLANKS), has(Items.JUNGLE_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.MANGROVE_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.MANGROVE_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.MANGROVE_PLANKS), has(Items.MANGROVE_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.DARK_OAK_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.DARK_OAK_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.DARK_OAK_PLANKS), has(Items.DARK_OAK_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.PALE_OAK_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.PALE_OAK_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.PALE_OAK_PLANKS), has(Items.PALE_OAK_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.BIRCH_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.BIRCH_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.BIRCH_STAIRS), has(Items.BIRCH_STAIRS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.CHERRY_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.CHERRY_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.CHERRY_PLANKS), has(Items.CHERRY_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.ACACIA_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.ACACIA_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.ACACIA_PLANKS), has(Items.ACACIA_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.CRIMSON_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.CRIMSON_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.CRIMSON_PLANKS), has(Items.CRIMSON_PLANKS))
+                        .save(output);
+                shaped(RecipeCategory.BUILDING_BLOCKS, Items.WARPED_STAIRS, 8)
+                        .pattern("P  ")
+                        .pattern("PP ")
+                        .pattern("PPP")
+                        .define('P', Items.WARPED_PLANKS)
+                        .group("Building")
+                        .unlockedBy(getHasName(Items.WARPED_PLANKS), has(Items.WARPED_PLANKS))
+                        .save(output);
             }
         };
     }
